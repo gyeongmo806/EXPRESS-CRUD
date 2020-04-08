4a0d1a9ca731436c32ef4e800c22dcba2dc558c4
gyeongmo_806 <jip080620@gmail.com>
2020-04-09 02:11:02 +0900

    read

--- app.js
+++ app.js
@@ -2,8 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const data = [
+  {
+    id: 1,
+    title:
+      "Hello 1",
+    des:
+      "example 1",
+  },
+  {
+    id: 2,
+    title:
+      "Hello 2",
+    des:
+      "example 2",
+  },
+];
+
+app.get('/api/readcontents',(req,res)=>{
+    res.send(data)
+})
+
+
 app.get('/',(req,res)=>{
-    res.send('Hello World')
+    res.sendfile('./index.html')
 })
 
 app.listen(port,()=>{
