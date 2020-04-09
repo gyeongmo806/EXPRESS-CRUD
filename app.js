4d8d984783ccfce6a69dc493514642021796caa2
gyeongmo_806 <jip080620@gmail.com>
2020-04-09 22:18:47 +0900

    add creat action

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const fs = require('fs')
 const port = 3000
 
-const data = [
-  {
-    id: 1,
-    title:
-      "Hello 1",
-    des:
-      "example 1",
-  },
-  {
-    id: 2,
-    title:
-      "Hello 2",
-    des:
-      "example 2",
-  },
-];
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+let data = [];
+
+fs.exists('./public/data.txt',function(exists){
+  console.log(exists ? "it'there" : opendata())
+  readdata()
+})
+readdata = ()=> {
+  console.log('readdata')
+  fs.readFile('./public/data.txt','utf8',(err,_data)=>{
+    if(err) throw err;
+    let json = JSON.parse(_data)
+    data = json
+  })
+
+}
 
 app.get('/api/readcontents',(req,res)=>{
-    res.send(data)
+    res.send(data) 
 })
 
+app.get('/create',(req,res)=>{
+  res.sendFile(__dirname+'/public/create.html')
+})
+
+app.post('/api/create',(req,res)=>{
+    const _id = data.length
+    data.push({id:_id,title:req.body.title,des:req.body.des})
+    console.log('data push')
+    fs.writeFile('./public/data.txt',JSON.stringify(data),'utf8',(err)=>{
+      if(err) throw err;
+      console.log('write complete')
+    })
+    res.redirect('/')
+})
 
 app.get('/',(req,res)=>{
-    res.sendfile('./index.html')
+    res.sendFile(__dirname+'/public/index.html')
 })
 
 app.listen(port,()=>{
